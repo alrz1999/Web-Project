7235c8f5ffe02c45606441d6f06fb533144f8e5a
abolfazl <asad.abolfazl.asad@gmail.com>
2021-02-17 10:37:40 +0330

    login page added

--- front/src/pages/MainRouter.js
+++ front/src/pages/MainRouter.js
@@ -1,30 +1,8 @@
-import React, { useContext, createContext, useState } from "react";
+import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation,
-} from "react-router-dom";
-import LogIn from "./LogIn";
-
-// This example has 3 pages: a public page, a protected
-// page, and a login screen. In order to see the protected
-// page, you must first login. Pretty standard stuff.
-//
-// First, visit the public page. Then, visit the protected
-// page. You're not yet logged in, so you are redirected
-// to the login page. After you login, you are redirected
-// back to the protected page.
-//
-// Notice the URL change each time. If you click the back
-// button at this point, would you expect to go back to the
-// login page? No! You're already logged in. Try it out,
-// and you'll see you go back to the page you visited
-// just *before* logging in, the public page.
+import { Switch, Route, useLocation } from "react-router-dom";
+import LogInForm from "../components/LogInForm";
+import SignPage from "../components/SignPage";
 
 export default function MainRouter() {
   let location = useLocation();
@@ -32,9 +10,10 @@ export default function MainRouter() {
   return (
     <Switch>
       <Route path="/login">
-        <LogIn />
+        <SignPage patientPage={<LogInForm />} doctorPage={<LogInForm />} />
       </Route>
-      <Route path="/signin"></Route>
+      <Route path="/signup">
+      </Route>
     </Switch>
   );
 }
